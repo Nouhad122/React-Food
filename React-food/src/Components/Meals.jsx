32da01689038141a200c09cde1ec20cf079050f1
Nouhad122 <nouhadalhallab122@gmail.com>
2025-01-07 17:05:02 +0300

    fetched data and created header and meal cards

---
+++ React-food/src/Components/Meals.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+import MealItem from './MealItem';
+
+const Meals = () => {
+    const [loadedMeals, setLoadedMeals] = useState([]);
+
+    useEffect(() =>{
+        const fetchMeals = async () =>{
+            const response = await fetch("http://localhost:3000/meals");
+
+            if(!response.ok){
+                return;
+            }
+            const meals = await response.json();
+            setLoadedMeals(meals);
+        }
+        fetchMeals();
+    },[]);
+
+  return (
+    <div>
+        <ul id='meals'>
+            {
+                loadedMeals.map(meal => <MealItem key={meal.id} meal={meal}/>)
+            }
+        </ul>
+    </div>
+  )
+}
+
+export default Meals
